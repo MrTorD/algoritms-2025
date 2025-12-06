93aaebd0abf31ef24daf11e76ac80a185f1a4625
MrTorD <justamail69420@gmail.com>
2025-12-06 04:22:32 +0300

    Добавлена генерация случайной последовательности чисел и вывод дерева в файл

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -7,10 +7,40 @@
 #include <conio.h>
 #include "BTree.h"
 
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+const int NUMBERS_COUNT = 5000000;
+const std::string NUMBERS_FILE_NAME = "5_numbers.txt";
+
+void generateShuffled(int count, std::string outName) {
+    std::vector<int> numbers(count);
+    std::iota(numbers.begin(), numbers.end(), 1);
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 rng(seed);
+
+    std::shuffle(numbers.begin(), numbers.end(), rng);
+
+    std::ofstream outFile(outName);
+    if (outFile.is_open()) {
+        for (int num : numbers) {
+            outFile << num << " "; 
+        }
+        outFile.close();
+    }
+}
+
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    std::cout << "Генерация чисел...";
+    generateShuffled(NUMBERS_COUNT, NUMBERS_FILE_NAME);
+    system("cls");
 
     std::cout << "Введите порядок дерева и название входного файла с числами\n";
     int level;
@@ -37,10 +67,11 @@ int main()
         system("cls");
         std::cout << "Введите:\n";
 
-        std::cout << "1. Вывести Б-дерево\n";
-        std::cout << "2. Добавить ключ\n";
-        std::cout << "3. Найти ключ\n";
-        std::cout << "4. Выход\n";
+        std::cout << "1. Вывести Б-дерево в консоль\n";
+        std::cout << "2. Вывести Б-дерево в файл\n";
+        std::cout << "3. Добавить ключ\n";
+        std::cout << "4. Найти ключ\n";
+        std::cout << "5. Выход\n";
 
         int ch = _getch();
 
@@ -54,6 +85,17 @@ int main()
             _getch();
             break;
         case '2':
+        {
+            system("cls");
+            std::cout << "Введите название файла и нажмите Enter\n";
+            std::string outName;
+            std::cin >> outName;
+            std::ofstream out(outName);
+            tree.print(out);
+            out.close();
+            break;
+        }
+        case '3':
             system("cls");
             std::cout << "Введите число и нажмите Enter\n";
             std::cin >> num;
@@ -61,7 +103,7 @@ int main()
             std::cout << "Нажмите любую клавишу для продолжения...";
             _getch();
             break;
-        case '3':
+        case '4':
             system("cls");
             std::cout << "Введите число и нажмите Enter\n";
             std::cin >> num;
@@ -75,7 +117,7 @@ int main()
             std::cout << "Нажмите любую клавишу для продолжения...";
             _getch();
             break;
-        case '4':
+        case '5':
             return 0; 
         }
     }
